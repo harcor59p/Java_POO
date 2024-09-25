2342fdba8abdead580d5c2e2ec6e2af1436928f1
Harold Cortes <harcor59p@gmail.com>
2024-09-25 08:12:52 -0500

    avance del ejercicio

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Principal {
+    public static void main(String[] args) {
+        Peliculas miPelicula = new Peliculas();
+        miPelicula.nombre = "Encanto" ;
+        miPelicula.fechaDeLanzamiento = 2021 ;
+        miPelicula.duracionEnMinutos = 120 ;
+
+
+        miPelicula.muestraFichaTecnica();
+
+
+
+        Peliculas otraPelicula = new Peliculas() ;
+
+        otraPelicula.nombre = "Matrix" ;
+        otraPelicula.fechaDeLanzamiento = 1999 ;
+        otraPelicula.duracionEnMinutos = 150 ;
+
+        otraPelicula.muestraFichaTecnica();
+    }
 }
