757fc9563df3f8a1a5cd7a01cfbd0f96b0dd8799
Harold Cortes <harcor59p@gmail.com>
2024-10-11 06:15:04 -0500

    impresion de listas

--- src/com/aluracursos/screenmatch/principal/PrincipalConLIstas.java
+++ src/com/aluracursos/screenmatch/principal/PrincipalConLIstas.java
@@ -9,15 +9,32 @@ import java.util.ArrayList;
 public class PrincipalConLIstas {
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula("Encanto" , 2021);
+        miPelicula.evalua(9);
         Serie casaDragon = new Serie("La casa del dragon" , 2022);
+        casaDragon.evalua(8.5);
         Pelicula otraPelicula = new Pelicula("Matrix" , 1999);
+        otraPelicula.evalua(9.5);
         var peliculaDeBruno = new Pelicula("El Señor de los Anillos" , 2001) ;
+        peliculaDeBruno.evalua(8);
+
+        Pelicula p1 = miPelicula ;
 
         ArrayList<Titulo> lista = new ArrayList<>() ;
         lista.add(miPelicula) ;
         lista.add(otraPelicula) ;
         lista.add(peliculaDeBruno) ;
         lista.add(casaDragon) ;
+
+        lista.forEach(System.out::println);
+
+
+        for (Titulo item: lista){
+            System.out.println(item.getNombre());
+            if (item instanceof Pelicula pelicula && pelicula.getClasificacion() > 2){
+            System.out.println(((Pelicula) item).getClasificacion());
+
+            }
+        }
 
     }
 }
